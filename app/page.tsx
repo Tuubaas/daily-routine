450ea0de51235529cef53fdf53fa78614e6a0e48
Tobias Lindgren <tuubbaas@gmail.com>
2023-10-20 22:18:51 +0200

    Make first page split equally

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export const dynamic = 'force-dynamic';
 export default function Page() {
   return (
     <main>
-      <div className="flex flex-row [&>div]:p-8 h-full">
+      <div className="flex flex-row [&>div]:p-8 h-full [&>div]:flex-1">
         <Today />
         <TodoList />
       </div>
