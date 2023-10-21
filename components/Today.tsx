e4fa048b9672a9ea3531953bf83e7c110ae359b6
Tobias Lindgren <tuubbaas@gmail.com>
2023-10-21 10:10:42 +0200

    Small bug fixes to makes text visible and not dupliacte

--- components/Today.tsx
+++ components/Today.tsx
@@ -3,10 +3,9 @@ import { db, CommitmentsTable } from '@/lib/drizzle';
 import type { Commitment } from '@/lib/drizzle';
 import { eq } from 'drizzle-orm';
 import dayjs from 'dayjs';
-import { seedCommitments } from '@/lib/seed';
 
 export default async function Today() {
-  let commitments: Commitment[];
+  let commitments: Commitment[] = [];
 
   try {
     commitments = await db
@@ -18,22 +17,28 @@ export default async function Today() {
       console.log(
         'Table does not exist, creating and seeding it with dummy data now...',
       );
-      // Table is not created yet
-      await seedCommitments();
-      commitments = await db.select().from(CommitmentsTable);
     } else {
       throw e;
     }
   }
+  console.log(commitments);
 
   return (
     <div className="flex flex-col h-full">
       <div className="bg-yellow-200 shadow-md">
-        <h1 className="mt-8 border-b border-gray-400 pl-8 pr-4">
-          <span>{dayjs().format('YYYY-MM-DD')}</span>
-        </h1>
+        <div className="px-4 flex flex-row justify-between border-b border-gray-400">
+          <h1 className="flex items-end ml-4">
+            <span>{dayjs().format('YYYY-MM-DD')}</span>
+          </h1>
+          <a
+            href="/commitment/update"
+            className="hover:bg-black/5 flex items-center px-4 my-4 border border-black rounded-2xl cursor-pointer"
+          >
+            Update
+          </a>
+        </div>
         <div className="mb-12">
-          {commitments.map((todaysCommitments) => (
+          {[commitments[0]].map((todaysCommitments) => (
             <ul
               key={todaysCommitments.id}
               className="flex flex-col [&>li]:h-8 [&>li]:border-b [&>li]:border-gray-400 [&>li]:pl-8 [&>li]:pr-4"
@@ -49,6 +54,14 @@ export default async function Today() {
             </ul>
           ))}
         </div>
+      </div>
+      <div className="flex justify-center">
+        <a
+          href="commitment"
+          className="mt-12 border border-black rounded-3xl px-12 py-4 text-xl hover:bg-black/5 cursor-pointer"
+        >
+          New
+        </a>
       </div>
     </div>
   );
